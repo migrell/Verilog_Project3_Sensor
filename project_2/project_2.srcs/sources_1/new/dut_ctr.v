82900a0be8abc729821917a05638b608b2ae66db
migrell <parkjiho333@naver.com>
2025-03-27 04:25:40 +0900

    타임 아웃 카운터 추가

--- project_2/project_2.srcs/sources_1/new/dut_ctr.v
+++ project_2/project_2.srcs/sources_1/new/dut_ctr.v
@@ -1,9 +1,13 @@
-module dut_ctr (
+
+// ----------------------
+// [3] DUT Controller (FSM)
+// ----------------------
+module dut_ctr(
     input clk,
     input rst,
     input btn_start,
     input tick_counter,
-    input btn_next,  // T17 핀에 연결될 추가 버튼 입력
+    input btn_next,
 
     output reg sensor_data,
     output reg [3:0] current_state,
@@ -21,17 +25,15 @@ module dut_ctr (
     output reg stop_out,
     output reg read
 );
-
-    localparam IDLE       = 4'b0000;
-    localparam START      = 4'b0001;
-    localparam WAIT       = 4'b0010;
-    localparam SYNC_LOW   = 4'b0011;
-    localparam SYNC_HIGH  = 4'b0100;
-    localparam DATA_SYNC  = 4'b0101;
-    localparam DATA_BIT   = 4'b0110;
-    localparam STOP       = 4'b0111;
-    localparam READ       = 4'b1000;
-
+    localparam IDLE = 4'b0000;
+    localparam START = 4'b0001;
+    localparam WAIT = 4'b0010;
+    localparam SYNC_LOW = 4'b0011;
+    localparam SYNC_HIGH = 4'b0100;
+    localparam DATA_SYNC = 4'b0101;
+    localparam DATA_BIT = 4'b0110;
+    localparam STOP = 4'b0111;
+    localparam READ = 4'b1000;
     localparam MAX_BITS = 3;
 
     reg [3:0] state;
@@ -40,15 +42,14 @@ module dut_ctr (
 
     reg [2:0] btn_sync;
     reg btn_edge;
-
     reg [2:0] next_btn_sync;
     reg next_btn_edge;
 
     reg [9:0] tick_count;
     reg [7:0] delay_counter;
+    reg [7:0] bit_timeout_counter;
     reg tick_prev;
 
-    // 엣지 감지 블록
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             btn_sync <= 3'b000;
@@ -57,19 +58,14 @@ module dut_ctr (
             next_btn_edge <= 1'b0;
             tick_prev <= 0;
         end else begin
-            // btn_start rising edge
             btn_sync <= {btn_sync[1:0], btn_start};
             btn_edge <= (btn_sync[1] & ~btn_sync[2]);
-
-            // btn_next (T17)
             next_btn_sync <= {next_btn_sync[1:0], btn_next};
             next_btn_edge <= (next_btn_sync[1] & ~next_btn_sync[2]);
-
             tick_prev <= tick_counter;
         end
     end
 
-    // FSM
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             state <= IDLE;
@@ -82,6 +78,7 @@ module dut_ctr (
             sensor_data <= 0;
             dnt_sensor_data <= 0;
             delay_counter <= 0;
+            bit_timeout_counter <= 0;
 
             idle <= 1'b1;
             start <= 1'b0;
@@ -105,6 +102,7 @@ module dut_ctr (
 
             if (tick_counter & ~tick_prev) begin
                 delay_counter <= delay_counter + 1;
+                bit_timeout_counter <= bit_timeout_counter + 1;
             end
 
             case (state)
@@ -112,118 +110,91 @@ module dut_ctr (
                     idle <= 1'b1;
                     dnt_io <= 1'b1;
                     current_state <= IDLE;
-
                     if (btn_edge) begin
                         state <= START;
                         delay_counter <= 0;
                         bit_count <= 0;
+                        bit_timeout_counter <= 0;
                     end
                 end
-
                 START: begin
                     start <= 1'b1;
                     dnt_io <= 1'b0;
                     current_state <= START;
-
                     if (delay_counter >= 50) begin
                         state <= WAIT;
                         delay_counter <= 0;
                     end
                 end
-
                 WAIT: begin
                     wait_state <= 1'b1;
                     dnt_io <= 1'b1;
                     current_state <= WAIT;
-
                     if (delay_counter >= 50) begin
                         state <= SYNC_LOW;
                         delay_counter <= 0;
                     end
                 end
-
                 SYNC_LOW: begin
                     sync_low_out <= 1'b1;
                     dnt_io <= 1'b0;
                     current_state <= SYNC_LOW;
-
                     if (delay_counter >= 50) begin
                         state <= SYNC_HIGH;
                         delay_counter <= 0;
                     end
                 end
-
                 SYNC_HIGH: begin
                     sync_high_out <= 1'b1;
                     dnt_io <= 1'b1;
                     current_state <= SYNC_HIGH;
-
                     if (delay_counter >= 50) begin
                         state <= DATA_SYNC;
                         delay_counter <= 0;
                     end
                 end
-
                 DATA_SYNC: begin
                     data_sync_out <= 1'b1;
                     dnt_io <= 1'b0;
                     current_state <= DATA_SYNC;
-
                     if (delay_counter >= 50) begin
                         state <= DATA_BIT;
                         delay_counter <= 0;
+                        bit_timeout_counter <= 0;
                     end
                 end
-
                 DATA_BIT: begin
                     data_bit_out <= 1'b1;
                     dnt_io <= 1'b1;
                     current_state <= DATA_BIT;
-
-                    if (next_btn_edge) begin
+                    if (next_btn_edge || bit_timeout_counter >= 30) begin
                         received_data <= {received_data[38:0], 1'b1};
                         sensor_data <= 1'b1;
                         bit_count <= bit_count + 1;
                         delay_counter <= 0;
+                        bit_timeout_counter <= 0;
                         state <= STOP;
                     end
-                    else if (delay_counter >= 50) begin
-                        received_data <= {received_data[38:0], 1'b1};
-                        sensor_data <= 1'b1;
-                        bit_count <= bit_count + 1;
-                        delay_counter <= 0;
-
-                        if (bit_count >= MAX_BITS - 1) begin
-                            state <= STOP;
-                        end else begin
-                            state <= DATA_SYNC;
-                        end
-                    end
                 end
-
                 STOP: begin
                     stop_out <= 1'b1;
                     dnt_io <= 1'b1;
                     current_state <= STOP;
-
                     if (delay_counter >= 100) begin
                         state <= READ;
                         dnt_sensor_data <= received_data[39:32];
                         delay_counter <= 0;
                     end
                 end
-
                 READ: begin
                     read <= 1'b1;
                     dnt_io <= 1'b0;
                     current_state <= READ;
-
                     if (delay_counter >= 50) begin
                         state <= IDLE;
                         delay_counter <= 0;
                     end
                 end
-
                 default: begin
                     state <= IDLE;
                     current_state <= IDLE;
@@ -231,8 +202,248 @@ module dut_ctr (
             endcase
         end
     end
-
 endmodule
+
+
+
+
+
+
+// module dut_ctr (
+//     input clk,
+//     input rst,
+//     input btn_start,
+//     input tick_counter,
+//     input btn_next,  // T17 핀에 연결될 추가 버튼 입력
+
+//     output reg sensor_data,
+//     output reg [3:0] current_state,
+//     output reg [7:0] dnt_data,
+//     output reg [7:0] dnt_sensor_data,
+//     output reg dnt_io,
+
+//     output reg idle,
+//     output reg start,
+//     output reg wait_state,
+//     output reg sync_low_out,
+//     output reg sync_high_out,
+//     output reg data_sync_out,
+//     output reg data_bit_out,
+//     output reg stop_out,
+//     output reg read
+// );
+
+//     localparam IDLE       = 4'b0000;
+//     localparam START      = 4'b0001;
+//     localparam WAIT       = 4'b0010;
+//     localparam SYNC_LOW   = 4'b0011;
+//     localparam SYNC_HIGH  = 4'b0100;
+//     localparam DATA_SYNC  = 4'b0101;
+//     localparam DATA_BIT   = 4'b0110;
+//     localparam STOP       = 4'b0111;
+//     localparam READ       = 4'b1000;
+
+//     localparam MAX_BITS = 3;
+
+//     reg [3:0] state;
+//     reg [5:0] bit_count;
+//     reg [39:0] received_data;
+
+//     reg [2:0] btn_sync;
+//     reg btn_edge;
+
+//     reg [2:0] next_btn_sync;
+//     reg next_btn_edge;
+
+//     reg [9:0] tick_count;
+//     reg [7:0] delay_counter;
+//     reg tick_prev;
+
+//     // 엣지 감지 블록
+//     always @(posedge clk or posedge rst) begin
+//         if (rst) begin
+//             btn_sync <= 3'b000;
+//             btn_edge <= 1'b0;
+//             next_btn_sync <= 3'b000;
+//             next_btn_edge <= 1'b0;
+//             tick_prev <= 0;
+//         end else begin
+//             // btn_start rising edge
+//             btn_sync <= {btn_sync[1:0], btn_start};
+//             btn_edge <= (btn_sync[1] & ~btn_sync[2]);
+
+//             // btn_next (T17)
+//             next_btn_sync <= {next_btn_sync[1:0], btn_next};
+//             next_btn_edge <= (next_btn_sync[1] & ~next_btn_sync[2]);
+
+//             tick_prev <= tick_counter;
+//         end
+//     end
+
+//     // FSM
+//     always @(posedge clk or posedge rst) begin
+//         if (rst) begin
+//             state <= IDLE;
+//             tick_count <= 0;
+//             bit_count <= 0;
+//             received_data <= 0;
+//             dnt_data <= 0;
+//             dnt_io <= 1'b1;
+//             current_state <= IDLE;
+//             sensor_data <= 0;
+//             dnt_sensor_data <= 0;
+//             delay_counter <= 0;
+
+//             idle <= 1'b1;
+//             start <= 1'b0;
+//             wait_state <= 1'b0;
+//             sync_low_out <= 1'b0;
+//             sync_high_out <= 1'b0;
+//             data_sync_out <= 1'b0;
+//             data_bit_out <= 1'b0;
+//             stop_out <= 1'b0;
+//             read <= 1'b0;
+//         end else begin
+//             idle <= 1'b0;
+//             start <= 1'b0;
+//             wait_state <= 1'b0;
+//             sync_low_out <= 1'b0;
+//             sync_high_out <= 1'b0;
+//             data_sync_out <= 1'b0;
+//             data_bit_out <= 1'b0;
+//             stop_out <= 1'b0;
+//             read <= 1'b0;
+
+//             if (tick_counter & ~tick_prev) begin
+//                 delay_counter <= delay_counter + 1;
+//             end
+
+//             case (state)
+//                 IDLE: begin
+//                     idle <= 1'b1;
+//                     dnt_io <= 1'b1;
+//                     current_state <= IDLE;
+
+//                     if (btn_edge) begin
+//                         state <= START;
+//                         delay_counter <= 0;
+//                         bit_count <= 0;
+//                     end
+//                 end
+
+//                 START: begin
+//                     start <= 1'b1;
+//                     dnt_io <= 1'b0;
+//                     current_state <= START;
+
+//                     if (delay_counter >= 50) begin
+//                         state <= WAIT;
+//                         delay_counter <= 0;
+//                     end
+//                 end
+
+//                 WAIT: begin
+//                     wait_state <= 1'b1;
+//                     dnt_io <= 1'b1;
+//                     current_state <= WAIT;
+
+//                     if (delay_counter >= 50) begin
+//                         state <= SYNC_LOW;
+//                         delay_counter <= 0;
+//                     end
+//                 end
+
+//                 SYNC_LOW: begin
+//                     sync_low_out <= 1'b1;
+//                     dnt_io <= 1'b0;
+//                     current_state <= SYNC_LOW;
+
+//                     if (delay_counter >= 50) begin
+//                         state <= SYNC_HIGH;
+//                         delay_counter <= 0;
+//                     end
+//                 end
+
+//                 SYNC_HIGH: begin
+//                     sync_high_out <= 1'b1;
+//                     dnt_io <= 1'b1;
+//                     current_state <= SYNC_HIGH;
+
+//                     if (delay_counter >= 50) begin
+//                         state <= DATA_SYNC;
+//                         delay_counter <= 0;
+//                     end
+//                 end
+
+//                 DATA_SYNC: begin
+//                     data_sync_out <= 1'b1;
+//                     dnt_io <= 1'b0;
+//                     current_state <= DATA_SYNC;
+
+//                     if (delay_counter >= 50) begin
+//                         state <= DATA_BIT;
+//                         delay_counter <= 0;
+//                     end
+//                 end
+
+//                 DATA_BIT: begin
+//                     data_bit_out <= 1'b1;
+//                     dnt_io <= 1'b1;
+//                     current_state <= DATA_BIT;
+
+//                     if (next_btn_edge) begin
+//                         received_data <= {received_data[38:0], 1'b1};
+//                         sensor_data <= 1'b1;
+//                         bit_count <= bit_count + 1;
+//                         delay_counter <= 0;
+//                         state <= STOP;
+//                     end
+//                     else if (delay_counter >= 50) begin
+//                         received_data <= {received_data[38:0], 1'b1};
+//                         sensor_data <= 1'b1;
+//                         bit_count <= bit_count + 1;
+//                         delay_counter <= 0;
+
+//                         if (bit_count >= MAX_BITS - 1) begin
+//                             state <= STOP;
+//                         end else begin
+//                             state <= DATA_SYNC;
+//                         end
+//                     end
+//                 end
+
+//                 STOP: begin
+//                     stop_out <= 1'b1;
+//                     dnt_io <= 1'b1;
+//                     current_state <= STOP;
+
+//                     if (delay_counter >= 100) begin
+//                         state <= READ;
+//                         dnt_sensor_data <= received_data[39:32];
+//                         delay_counter <= 0;
+//                     end
+//                 end
+
+//                 READ: begin
+//                     read <= 1'b1;
+//                     dnt_io <= 1'b0;
+//                     current_state <= READ;
+
+//                     if (delay_counter >= 50) begin
+//                         state <= IDLE;
+//                         delay_counter <= 0;
+//                     end
+//                 end
+
+//                 default: begin
+//                     state <= IDLE;
+//                     current_state <= IDLE;
+//                 end
+//             endcase
+//         end
+//     end
+
+// endmodule
 
 
 
--- project_2/project_2.srcs/sources_1/new/tick_gen.v
+++ project_2/project_2.srcs/sources_1/new/tick_gen.v
@@ -1,10 +1,14 @@
+
+// ----------------------
+// [2] Tick Generator
+// ----------------------
 module tick_gen(
     input clk,
     input rst,
     output reg tick_10msec
 );
     reg [19:0] tick_counter;
-    
+
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             tick_counter <= 0;
@@ -12,11 +16,9 @@ module tick_gen(
         end else begin
             if (tick_counter >= 1_000_000 - 1) begin  // 10ms 주기
                 tick_counter <= 0;
-                tick_10msec <= 1;  // 펄스 시작
+                tick_10msec <= 1;
             end else begin
                 tick_counter <= tick_counter + 1;
-                
-                // 펄스 유지 시간을 10 클럭 사이클로 설정
                 if (tick_counter < 10)
                     tick_10msec <= 1;
                 else
@@ -25,3 +27,33 @@ module tick_gen(
         end
     end
 endmodule
+
+
+
+// module tick_gen(
+//     input clk,
+//     input rst,
+//     output reg tick_10msec
+// );
+//     reg [19:0] tick_counter;
+    
+//     always @(posedge clk or posedge rst) begin
+//         if (rst) begin
+//             tick_counter <= 0;
+//             tick_10msec <= 0;
+//         end else begin
+//             if (tick_counter >= 1_000_000 - 1) begin  // 10ms 주기
+//                 tick_counter <= 0;
+//                 tick_10msec <= 1;  // 펄스 시작
+//             end else begin
+//                 tick_counter <= tick_counter + 1;
+                
+//                 // 펄스 유지 시간을 10 클럭 사이클로 설정
+//                 if (tick_counter < 10)
+//                     tick_10msec <= 1;
+//                 else
+//                     tick_10msec <= 0;
+//             end
+//         end
+//     end
+// endmodule
