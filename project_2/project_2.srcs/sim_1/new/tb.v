6aa0d13cb005d824b90e5e6a9f954716712215fc
migrell <parkjiho333@naver.com>
2025-03-26 14:38:57 +0900

    테스트 벤치 작성

--- project_2/project_2.srcs/sim_1/new/tb.v
+++ project_2/project_2.srcs/sim_1/new/tb.v
@@ -1,39 +1,55 @@
 `timescale 1ns / 1ps
 
+module tb_dht11();
+  reg clk;
+  reg reset;
+  reg btn_start;
+  
+  reg dht_sensor_data;
+  reg io_oe;
+  
+  wire led;
+  wire dht_io;
+  
+  assign dht_io = (io_oe) ? dht_sensor_data : 1'bz;
+  
+  dht11 dut (
+    .clk(clk),
+    .reset(reset),
+    .btn_start(btn_start),
+    .led(led),
+    .dht_io(dht_io)
+  );
+    
+  always #5 clk = ~clk;
+  
+  initial begin
+    clk = 0;
+    reset = 1;
+    io_oe = 0;
+    btn_start = 0;
+    
+    #100;
+    reset = 0;
 
-module tb_dht11(
+    #100;
+    btn_start = 1;
 
-reg clk;
-reg reset;
-reg btn_start;
+    #100;
+    btn_start = 0;
 
+    wait(dht_io);
+    #30000; //입력 모드 변환
 
-reg dht_sensor_data;
-reg io_oe;
+    io_oe = 1;
+    dht_sensor_data = 1'b0;
 
-wire led;
-wire dht_io;
-
-dht11 dut (
-    .clk,
-    .reset,
-
-
-
-
-
-);
-
-
-
-
-
-
-
-
-
-
-
-
-    );
+    #80000;
+    
+    dht_sensor_data = 1'b1;
+    #80000;
+    #50000;
+    $stop;
+  end
+  
 endmodule
