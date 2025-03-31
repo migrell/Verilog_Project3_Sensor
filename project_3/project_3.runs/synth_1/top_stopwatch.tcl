d9dcdc4ce4645492c40037dbcb21075c88c4743c
migrell <parkjiho333@naver.com>
2025-03-31 14:18:10 +0900

    협업을하자 시뮬레이션을 하자

--- project_3/project_3.runs/synth_1/top_stopwatch.tcl
+++ project_3/project_3.runs/synth_1/top_stopwatch.tcl
@@ -70,9 +70,7 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param chipscope.maxJobs 4
-set_param synth.incrementalSynthesisCache C:/Users/kccistc/Desktop/Verilog_Project3_Sensor/project_3/.Xil/Vivado-7680-DESKTOP-7CFQ9ND/incrSyn
-set_param xicom.use_bs_reader 1
+set_param synth.incrementalSynthesisCache C:/Users/kccistc/Desktop/Verilog_Project3_Sensor/project_3/.Xil/Vivado-7476-DESKTOP-7CFQ9ND/incrSyn
 set_msg_config -id {Synth 8-256} -limit 10000
 set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
